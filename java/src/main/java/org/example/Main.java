c65389f2f7b4ded3fd0c1e243f9b0ffbc4a91207
John Wood <113411200+john-h-wood@users.noreply.github.com>
2024-02-12 20:51:58 -0500

    Started Maven project

---
+++ java/src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+import java.math.BigDecimal;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+            BigDecimal x = new BigDecimal("10.1");
+            System.out.println(x.toString().toString().toString());
+        }
+    }
